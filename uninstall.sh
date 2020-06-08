8b7620c0274f998ddeb176dc7e069be918db0c69
Carlos Luque Córdoba <i62lucoc@uco.es>
2020-06-09 00:12:47 +0200

    Adding permission

--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # If not in VGG19demo-evaluator/ do
-cd /i62lucoc/
+cd ./i62lucoc/
 
 # Remove pipenv environment
 pipenv --rm
 
 # Remove project
 cd ../../
-rm -r VGG19demo-evaluator/
+rm -rf VGG19demo-evaluator/
 
 # Self destroy this bash file
 echo -e 'Project removed succesfully!'
-rm -- "$0"
