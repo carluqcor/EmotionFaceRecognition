deb0a256e11bab38cf56b9f7c8e04566768066b3
Carlos Luque Córdoba <i62lucoc@uco.es>
2020-06-08 22:37:37 +0200

    Uninstalling script

---
+++ uninstall.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# If not in VGG19demo-evaluator/ do
+cd ~/VGG19demo-evaluator/
+
+# Remove pipenv environment
+pipenv --rm
+
+# Remove project
+cd ..
+rm -r VGG19demo-evaluator/
+
+# Self destroy this bash file
+rm -- "$0"
