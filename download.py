6020c49e19be00286b1a6f457274084fd7c65f25
Carlos Luque Córdoba <i62lucoc@uco.es>
2020-06-08 22:41:49 +0200

    Change path folder name to avoid problems when removing pipenv

--- download.py
+++ download.py
@@ -45,6 +45,6 @@ with ZipFile('demo.zip', 'r') as zipObj:
     zipObj.extractall()
     os.remove('demo.zip')
 
-shutil.move('ficheros/', 'VGG19demo-evaluator/')
+shutil.move('i62lucoc/', 'VGG19demo-evaluator/')
     
 print("Files downloaded!")
