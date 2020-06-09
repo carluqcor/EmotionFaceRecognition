8c90b1a7dee4d1a94bd06cd06a304c95725e8223
Carlos Luque Córdoba <i62lucoc@uco.es>
2020-06-09 23:11:56 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-# If not in VGG19demo-evaluator/ do
-cd ./i62lucoc/
-
 # Remove pipenv environment
 pipenv --rm
 
 # Remove project
-cd ../../
-rm -rf VGG19demo-evaluator/
+cd ../
+rm -rf EmotionFaceRecognition/
 
-# Self destroy this bash file
 echo -e 'Project removed succesfully!'
